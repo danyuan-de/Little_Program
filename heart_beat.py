c946aac0290b245e9218622abda0bf538aaedc02
DanYuDE <nadal3815@gmail.com>
2023-06-25 01:12:45 +0200

    Updated code on Ubuntu 25-06-2023

--- heart_beat.py
+++ heart_beat.py
@@ -10,7 +10,7 @@ class ParticlePrinciple:
         self.size = size
         self.offset = offset # random offset factor for the particle
 
-    def draw_particle(self):
+    def draw_particle(self, screen):
         pos_x = 10 * self.offset * self.cur_pos[0] + 300
         pos_y = -10 * self.offset * self.cur_pos[1] + 180
         size_x = self.size[0]
@@ -22,20 +22,24 @@ class ParticlePrinciple:
         pass
 
 # --------------------------------- change here ---------------------------------
-class heart:
+class HeartAnimation:
     def __init__(self) -> None:
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("Heart Beat")
         self.particles = []
         
-    def heart_curve(self, num_particles=20000, theta=0):
-        t_interval = 2 * pi / num_particles # 2 * pi = 1 T cycle, divided by num of particles to calculate time interval of each particle
-        
-        # c = 0
+    def generate_heart_particles(self):
+        num_particles = 10000
+        t_interval = 2 * pi / num_particles
+        theta = 0
+        i = 0
 
         # Generate particles on the heart curve
         while theta < 2 * pi:
-            # c += 1 
-            # sigma = 0.15 if c % 5 else 0.3
-            # f = 1 - abs(random.gauss(1, sigma) - 1)
+            i += 1
+            sigma = 0.15 if i % 5 else 0.3
+            f = 1 - abs(random.gauss(1, sigma) - 1) # stochastic shifting proportion
             offset = 1
 
             # -------------------------------- heart curve --------------------------------
@@ -43,40 +47,37 @@ class heart:
             y = 13 * cos(theta) - 5 * cos(2 * theta) - 2 * cos(3 * theta) - cos(4 * theta)
             # -----------------------------------------------------------------------------
             
-            size = (random.uniform(0.9, 2.5), random.uniform(0.9, 2.5)) # randomly change the size of every particle
+            size = (random.uniform(1, 2.5), random.uniform(1, 2.5)) # randomly change the size of every particle
             self.particles.append(ParticlePrinciple((x, y), size, offset))
             theta += t_interval
 
-    def draw(self):
+    def draw_Ani(self, screen):
         screen.fill((0, 0, 0))
         # Draw the heart particles
         for p in self.particles:
-            p.draw_particle()
+            p.draw_particle(screen)
+
+    def update_Ani(self):
+        pass 
+
+    def run(self):
+        while True:
+            self.update_Ani()
+            self.draw_Ani(self.screen)
+            for event in pygame.event.get(): 
+                if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+
+            pygame.display.update()
+            self.clock.tick(120)
 
 # -------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    clock = pygame.time.Clock()
+    heart_beat = HeartAnimation()
+    heart_beat.run()
 
-    TITLE = "Heart Beat"
-    pygame.display.set_caption(TITLE)
-    pygame.display.get_surface
     
-    # particle = ParticlePrinciple()
-
-    while True:
-        for event in pygame.event.get(): 
-            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-
-        # ------ change here ------
-        particles = ParticlePrinciple()
-
-        particles.draw()
-        # -------------------------
-        pygame.display.update()
-        clock.tick(120)
